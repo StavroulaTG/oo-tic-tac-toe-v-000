97c89f8f6337e2ac374d139dd2dda80146d267e1
Stavroula Guerrero <4stacyg@optonline.net>
2017-12-21 20:31:33 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -86,9 +86,8 @@ class TicTacToe
   def play
     until over?
       turn
-      end
-      if won? puts "Congratulations #{winner}!"
-        elsif draw? puts "Cat's Game!"
+    end
+    if won? puts "Congratulations #{winner}!"
+    elsif draw? puts "Cat's Game!"
     end
   end
-end
